8b3ae2b7168642730d529b9eb665c464247aa566
mansurisaad555 <mansurisaad555@gmail.com>
2024-09-24 19:42:33 -0500

    new feature

--- feature.java
+++ feature.java
@@ -2,6 +2,6 @@ public class feature {
     public static void main(String[] args) {
         System.out.println("branching feature3");
         System.out.println("conflict 1");
-          
+        System.out.println("new feature");
     }
 }
